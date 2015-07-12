42e20fce26c8100879201c8390731d77b95ffa90
kendobi <yokenny@gmail.com>
2015-07-12 12:50:23 -0700

    adding fade in/out capabilities

---
+++ Assets/Standard Assets/Scripts/FadeInOut.js
@@ -0,0 +1,21 @@
+﻿ var track1 : AudioClip;
+ 
+ GetComponent.<AudioSource>().clip = track1;
+ 
+ var audio1Volume : float = 1.0;
+ 
+ 
+ function fadeIn() {
+     if (audio1Volume < 1) {
+         audio1Volume += 0.5 * Time.deltaTime;
+         GetComponent.<AudioSource>().volume = audio1Volume;
+     }
+ }
+ 
+ function fadeOut() {
+     while (audio1Volume > 0.1) {
+         audio1Volume -= 0.5 * Time.deltaTime;
+         GetComponent.<AudioSource>().volume = audio1Volume;
+         yield;
+     }
+ }
